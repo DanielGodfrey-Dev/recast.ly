8851f1a3104dd4a4a0a804cdeb7f4394a72fbd8d
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-19 09:40:25 -0800

    changed to stateful class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,57 @@ import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoPlayer from './VideoPlayer.js';
 
-var App = () => (
-  <div>
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div><h5><em>videoPlayer</em><VideoPlayer video={exampleVideoData[0]}/>
+//         </h5></div>
+//       </div>
+//       <div className="col-md-5">
+//         <div><h5><em>videoList</em> <VideoList videos={exampleVideoData}/> </h5></div>
+//       </div>
+//     </div>
+//   </div>
+// );
 
 
+class App extends React.Component {
+  constructor() {
+    super(),
+    this.state = {
+      videos: exampleVideoData,
+      video: exampleVideoData[0]
+    };
+  }
 
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em>videoPlayer</em><VideoPlayer video={this.state.video}/>
+            </h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em>videoList</em> <VideoList videos={this.state.videos}/> </h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em><VideoPlayer video={exampleVideoData}/>
-        </h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> <VideoList videos={exampleVideoData}/> </h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
