4484ef45cae2a1fe0c9d6e5bbed2dccd1f9efde1
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-02-18 18:02:55 -0800

    finsihed with the videoListentry quests

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -4,21 +4,12 @@ import VideoListEntry from './VideoListEntry.js';
 var VideoList = (props) => (
 
   // console.log(props.videos[0][1]),
+
+
   <div className="video-list">
-    <div><h5><em>videoListEntry <VideoListEntry video={props.videos[0][0]}/></em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-
-    {/*
-    {props.videos[0].map(video =>
-
-      console.log(video)
-      // <VideoListEntry key={etag} video = {video}/>
-    )} */}
-
-
+    {props.videos.map((a, idx) =>
+      <VideoListEntry key={idx} video = {a}/>
+    )}
   </div>
 );
 
